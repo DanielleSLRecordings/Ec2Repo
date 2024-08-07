ed6280636921af78e3c19cc8048a552ac026bdda
DanielleSLRecordings <164554798+DanielleSLRecordings@users.noreply.github.com>
2024-08-07 11:26:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # You can choose a different AMI
+  ami           = "ami-0a2202cf4c36161a1" 
   instance_type = "t2.micro"
 
   tags = {
